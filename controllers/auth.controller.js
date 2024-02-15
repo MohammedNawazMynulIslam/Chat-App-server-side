b17436e869b3c0f4fc63ea087d73f6a13d59af2a
MohammedNawazMynulIslam <mynulislam183@gmail.com>
2024-02-15 14:10:07 +0600

    added controller for signup login and logout

---
+++ controllers/auth.controller.js
@@ -0,0 +1,9 @@
+export const signup = (req,res)=>{
+    console.log("signupUser");
+}
+export const login = (req,res)=>{
+    console.log("login");
+}
+export const logout = (req,res)=>{
+    console.log("logout");
+}
