514f7f9a06b3cd4885a84b2b7c31d55bba879d99
MohammedNawazMynulIslam <mynulislam183@gmail.com>
2024-02-21 21:01:37 +0600

    added protected route

--- controllers/message.controller.js
+++ controllers/message.controller.js
@@ -1,8 +1,36 @@
-export const sendMessage =async(req,res)=>{
+import Conversation from "../models/conversation.model.js"
+import Message from "../models/message.model.js"
+const sendMessage = async (req,res)=>{
      try {
-
+      const {message} = req.body
+      const{ id: receiverId }= req.params;
+      const senderId = req.user._id
+      //create a new message and save it to the database
+      let conversation = await Conversation.findOne({
+         participants:{$all:[senderId, receiverId]},
+      })
+      if(!conversation){
+         conversation = await Conversation.create({
+            participants:[senderId, receiverId],
+         })
+      }
+      const newMessage = new Message({
+        senderId,
+        receiverId,
+        message
+      })
+      if(message){
+         conversation.messages.push(newMessage._id)
+      }
+      // await conversation.save()
+      // await newMessage.save()
+      // this will run in parallel
+      await Promise.all([conversation.save(), newMessage()])
+      res.status(201).json(newMessage);
      } catch (error) {
         console.log("Error in sendMessage controller", error.message )
         res.status(500).json({error:"Internal server error"});
      }
 };
+
+export  default sendMessage ;
--- server.js
+++ server.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv'
 import messageRoutes  from './routes/message.routes.js';
 import authRoutes from "./routes/auth.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
-
+import cookieParser from "cookie-parser";
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -11,6 +11,7 @@ dotenv.config();
 
 // middle ware
 app.use(express.json())  // parse request body as JSON payloads (from req.body)
+app.use(cookieParser());   // parses cookies and assigns them to req.cookies
  app.use("/api/auth", authRoutes)
  app.use("/api/messages", messageRoutes);
 
